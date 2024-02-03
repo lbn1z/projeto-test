5d965c0e226fe92d81f8ae91cdc5903d95a4fb5d
lbn1z <158527500+lbn1z@users.noreply.github.com>
2024-02-03 19:54:17 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+console.log(document);
+
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
